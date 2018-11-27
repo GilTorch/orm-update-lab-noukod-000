edeb7d0a58808fcfd641a73b162db361403c2fc6
Gilbert Torchon <gilbert@noukod.com>
2018-11-27 20:31:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,11 +54,11 @@ class Student
   end
 
   def self.find_by_name(name)
-    sql=<<-SQL 
-      SELECT * FROM students 
+    sql=<<-SQL
+      SELECT * FROM students
       WHERE name=?
     SQL
 
-    DB[:conn].execute(sql,name)
+    DB[:conn].execute(sql,name).first
   end
 end
