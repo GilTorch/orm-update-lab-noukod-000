a0c90c08c558c064ad009db1234f638add912aff
Gilbert Torchon <gilbert@noukod.com>
2018-11-27 20:31:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,4 +52,13 @@ class Student
     binding.pry
     Student.new(row[1],row[2],row[0])
   end
+
+  def self.find_by_name(name)
+    sql=<<-SQL 
+      SELECT * FROM students 
+      WHERE name=?
+    SQL
+
+    DB[:conn].execute(sql,name)
+  end
 end
